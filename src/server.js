0782603ee7702c936170d8bad4b03c241538d7e8
Weiming Zhai <wmzhai@gmail.com>
2015-12-28 14:09:30 +0800

    init server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+console.log("Hello Nodejs");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
